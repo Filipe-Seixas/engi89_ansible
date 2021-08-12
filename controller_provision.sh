3c960a195de75a516cee7107c5cce7e51e90828d
Filipe Silva <filipe.seixas@protonmail.com>
2021-08-12 14:17:15 +0100

    presentation and playbooks

--- controller_provision.sh
+++ controller_provision.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Install Dependencies
 sudo apt-get update -y
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
+# Install Ansible and tree
 sudo apt-get install ansible -y
 ansible --version
 sudo apt-get install tree -y
+# Install AWS dependencies
+sudo apt install python3-pip
+sudo pip3 install awscli
+sudo pip3 install boto boto3
+sudo apt-get update -y
+sudo apt-get upgrade -y
